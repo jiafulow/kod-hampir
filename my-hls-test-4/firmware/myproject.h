1d0cf75851d7a21fdb1859343e62ecc63f6750ba
Jia Fu Low <jia.fu.low@cern.ch>
2020-07-29 11:27:25 -0500

    Using arbitrary precision types

--- my-hls-test-4/firmware/myproject.h
+++ my-hls-test-4/firmware/myproject.h
@@ -48,14 +48,24 @@ ALL TIMES.
 #include "window_fn_class.h"
 
 // Test parameters
-#define FLOAT_DATA // Used to select error tolerance in test program
+//#define FLOAT_DATA // Used to select error tolerance in test program
 #define WIN_TYPE xhls_window_fn::HANN
 #define WIN_LEN 32
 
-// Define floating point types for input, output and window coefficients
-typedef float win_fn_in_t;
-typedef float win_fn_out_t;
-typedef float win_fn_coef_t;
+// Types and top-level function prototype
+#include <ap_int.h>
+// Define widths of fixed point fields
+#define W_IN    8
+#define IW_IN   8
+#define W_OUT   24
+#define IW_OUT  8
+#define W_COEF  18
+#define IW_COEF 2
+
+// Define fixed  point types for input, output and coefficients
+typedef ap_fixed<W_IN,IW_IN> win_fn_in_t;
+typedef ap_fixed<W_OUT,IW_OUT> win_fn_out_t;
+typedef ap_fixed<W_COEF,IW_COEF> win_fn_coef_t;
 
 // Top level function prototype - wraps all object, method and functions for HLS
 void myproject(win_fn_out_t outdata[WIN_LEN], win_fn_in_t indata[WIN_LEN]);
