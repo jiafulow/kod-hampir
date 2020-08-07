ba3b1364b76891ccc0a7b1c8d7ca52d4723e4be5
Jia Fu Low <jia.fu.low@cern.ch>
2020-08-06 23:52:35 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 DEST="$1"
 
-ORIG="my-hls-test-1"
+ORIG="my-hls-test-11"
 
 mkdir -p $DEST
 mkdir -p $DEST/firmware
@@ -13,3 +15,7 @@ cp $ORIG/build_prj.tcl $DEST
 > $DEST/firmware/weights/.gitkeep
 > $DEST/myproject_test.cpp
 > $DEST/tb_data/.gitkeep
+
+pushd $DEST/firmware
+ln -s ../../external/hls/2019.08-bcolbf/include/ ap_types
+popd
